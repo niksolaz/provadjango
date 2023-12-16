7a454ee35e58c71342152a24aaed3f9f9a57876e
Nicola Solazzo <solazzo.nicola@gmail.com>
2023-12-16 15:18:38 +0100

    create blog starter

--- mysite/blogs/models.py
+++ mysite/blogs/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/', default='images/me.png')
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    ## Quit and manually define a default value in models.py. for example: 
+    ## comment = models.TextField(default='No comment')
 
     def __str__(self):
         return self.title
--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,9 +1,26 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Blog
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the blogs index.")
+    list_blog = Blog.objects.all()
+    template = 'blogs/index.html'
+    context = {
+        'list_blog': list_blog
+    }
+    return render(request, template, context)
+    
 
 def detail(request, blog_id):
-    return HttpResponse("You're looking at blog %s." % blog_id)
+    try:
+        blog = get_object_or_404(Blog, pk=blog_id)
+        template = 'blogs/detail.html'
+        context = {
+            'blog': blog,
+        }
+    except Blog.DoesNotExist:
+        raise Http404("Blog does not exist")
+    return render(request, template, context)
 
