0e34402a6ae7faac642441c88997bf1c613335da
Nicola Solazzo <solazzo.nicola@gmail.com>
2023-12-28 19:40:40 +0100

    fixed test and create files for css template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Question, Choice
 from django.db.models import Sum, F
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -11,17 +14,24 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
     
 # Create your views here.
 def index(request):
