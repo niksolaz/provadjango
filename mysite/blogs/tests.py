6b85e1e43793f6c17f7c979a9bac48c10928c6de
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-01-07 15:57:01 +0100

    future pub_date test for blog

--- mysite/blogs/tests.py
+++ mysite/blogs/tests.py
@@ -1,3 +1,20 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Blog
 
 # Create your tests here.
+class BlogModelTests(TestCase):
+    
+    def test_pub_date_pretty_with_future_blog(self):
+        """
+        pub_date_pretty() returns the pub_date with future blog.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_blog = Blog(pub_date=time)
+        self.assertIs(future_blog.pub_date_pretty() != '30 Dec 2018, 00:00', True)
+    
+    
